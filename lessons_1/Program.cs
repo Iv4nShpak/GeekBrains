da99f1acd5e18a2620ae0b8e34512302c769971b
IShpak <llutar@inbox.ru>
2022-01-18 23:30:27 +0300

    add lessons_1

---
+++ lessons_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lessons_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите своё имя: ");
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"Привет {name}, сегодня {DateTime.Now}.");
+
+            Console.ReadKey();
+        }
+    }
+}
