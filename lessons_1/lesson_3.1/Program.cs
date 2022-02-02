96356e992279ce67be2fed6781c2aca40d00c35b
Ivan Shpak <llutar@inbox.ru>
2022-02-02 23:26:06 +0300

    add lesson_3.1-3.4

---
+++ lessons_1/lesson_3.1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace lesson_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numbersArray = new int [5, 5] 
+            {
+                {3,7,4,9,3},
+                {5,1,8,4,6},
+                {7,5,5,3,2},
+                {9,6,2,1,1},
+                {2,6,9,3,7}
+            };
+             
+
+            for (int i = 0; i < numbersArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < numbersArray.GetLength(1); j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write(numbersArray[i, j] + " ");
+                    }
+                }
+            }
+            
+            
+        }
+    }
+}
