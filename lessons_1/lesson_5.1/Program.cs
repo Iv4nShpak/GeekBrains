8f19b38ef6c0b88dc3934b450ec69b5b298f8144
Ivan Shpak <llutar@inbox.ru>
2022-02-14 19:00:30 +0300

    add lesson_5.1-5.3

---
+++ lessons_1/lesson_5.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace lesson_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userText = Console.ReadLine();
+            File.WriteAllText("User_Text.txt", userText);
+        }
+    }
+}
---
+++ lessons_1/lesson_5.3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace lesson_5._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите произвольный набор чисел от 0 до 255:");
+            string typingNumber = Console.ReadLine();
+            
+            string[] numberArray = typingNumber.Split();
+            byte[] numberArrayBytes = new byte[100];
+            
+            for (int i = 0; i < numberArray.Length; i++)
+            {
+                numberArrayBytes[i] = Convert.ToByte(numberArray[i]);
+            }
+            
+            File.WriteAllBytes("bytes.bin", numberArrayBytes);
+        }
+
+    }
+}
