37b133fc73a2a20efee5c3e9092374561aa88359
Ivan Shpak <llutar@inbox.ru>
2022-02-21 23:14:22 +0300

    add lesson_7

---
+++ lessons_1/lesson_7/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("D58EC373-E27B-4408-B055-F2077CCD77D3")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
