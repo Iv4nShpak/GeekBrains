3e56346c802b88b44a42da894ae7f80e368dac4b
Ivan Shpak <llutar@inbox.ru>
2022-02-28 23:46:35 +0300

    add lesson_8

---
+++ lessons_1/lesson_8/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace lesson_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string welcome = Properties.Settings.Default.Hello;
+            Console.WriteLine(welcome);
+
+            try
+            {
+                if (string.IsNullOrEmpty(Properties.Settings.Default.NameUser))
+                {
+                    Console.Write("Введите имя: ");
+                    Properties.Settings.Default.NameUser = Console.ReadLine();
+                    Properties.Settings.Default.Save();
+                }
+                if (Properties.Settings.Default.AgeUser == 0)
+                {
+                    Console.Write("Введите возраст: ");
+                    Properties.Settings.Default.AgeUser = int.Parse(Console.ReadLine());
+                    Properties.Settings.Default.Save();
+                }
+                if (string.IsNullOrEmpty(Properties.Settings.Default.TypeOfActivity))
+                {
+                    Console.Write("Введите род деятельности: ");
+                    Properties.Settings.Default.TypeOfActivity = Console.ReadLine();
+                    Properties.Settings.Default.Save();
+                }
+
+                string nameUser = Properties.Settings.Default.NameUser;
+                int ageUser = Properties.Settings.Default.AgeUser;
+                string typeOfActivity = Properties.Settings.Default.TypeOfActivity;
+                Console.WriteLine($"Пользоваетль: {nameUser}\nВозраст: {ageUser}\nРод деятельсности: {typeOfActivity}");
+                Console.ReadKey();
+
+                Console.WriteLine("Для удаления пользователя введите команду: delete");
+                string deleteUser = Console.ReadLine();
+                if (deleteUser == "delete")
+                {
+                    Properties.Settings.Default.NameUser = "";
+                    Properties.Settings.Default.AgeUser = 0;
+                    Properties.Settings.Default.TypeOfActivity = "";
+                    Properties.Settings.Default.Save();
+                }
+
+            }
+            catch (Exception exeption)
+            {
+                Console.WriteLine(exeption.Message);
+            }
+        }
+    }
+}
---
+++ lessons_1/lesson_8/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("lesson_8")]
+[assembly: AssemblyDescription("Данное приложение запрашивает у пользователя имя, возраст и род деятельности, а затем сохраняет данные в настройках. При следующем запуске отображает эти данные.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Dante")]
+[assembly: AssemblyProduct("lesson_8")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("a8a12fa1-7719-430c-b61e-4958f81f1d5e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
